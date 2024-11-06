0d9faefa1b4f86d531cdbc4fffba5b10b5af0e56
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-11-06 19:55:05 +0100

    add vercel analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { inject } from '@vercel/analytics';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({ mode: isDevMode() ? 'development' : 'development' });
+      },
+    },
+  ],
 };
