99330c1deed4c8e4ff41c43cec0337a77c7b96b9
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-11-06 23:44:03 +0100

    add roll doce

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.css'
+  styleUrl: './board.component.css',
 })
 export class BoardComponent {
+  diceRollsCount = 0;
+  animationState = 'start';
 
+  rollDice() {
+    this.diceRollsCount = Math.floor(Math.random() * 6) + 1;
+    console.log('Dice roll:', this.diceRollsCount);
+  }
 }
